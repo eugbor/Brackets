0a348cd6293b40ce3d2226486f3043ac7c35755d
eugbor <eugeneborodin@mail.ru>
2016-09-20 23:01:56 +0300

    commit

--- Brackets/Program.cs
+++ Brackets/Program.cs
@@ -14,34 +14,61 @@ namespace Brackets
 
         public static void GetNumber(int n)
         {
-            int [] nArray = new int[n];
-            for (int i = 0; i < nArray.Length; i++)
-                nArray[i] = 0;
-            
-            for (int i = 0; i < nArray.Length-1; i++)
+            int[] nArray1 = new int[n];
+            for (int i = 1; i < nArray1.Length; i++)
             {
-                nArray[i] = nArray[i] + 1;
-                GetArray(nArray);
-                
-                for (int j = i+1; j < nArray.Length-1; j++)
+                nArray1[i] = 0;
+            }
+            int[] nArray2 = new int[n];
+            for (int i = 1; i < nArray2.Length; i++)
+            {
+                nArray2[i] = 0;
+            }
+            nArray2[0] = 1;
+            int sum = 0;
+            while (sum != n)
+            {
+                for (int i = 0; i < nArray1.Length; i++)
                 {
-                    nArray[j] = nArray[j] + 1;
-                    GetArray(nArray);
-                    nArray[j] = 0;
-
+                    nArray1[i] = nArray1[i] + nArray2[i];
+                    if (nArray1[nArray1.Length - 1] == 2) {nArray1[nArray1.Length - 1]=0;}
+                    if (nArray1[i] == 2)
+                    {
+                        nArray1[i] = 0;
+                        nArray1[i + 1] = nArray1[i + 1] + 1;
+                    }
+                }
+                GetArray(nArray1);
+                sum = 0;
+                for (int i = 0; i < nArray1.Length; i++)
+                {
+                    sum += nArray1[i];
                 }
             }
         }
 
-        
         public static void GetArray(int[] list)
         {
-            int sum = 0;
+            int sum1 = 0;
             for (int i = 0; i < list.Length; i++)
             {
-                sum += list[i];
+                sum1 += list[i];
             }
-            if (sum != list.Length/2){return;}
+            
+            if (sum1 != list.Length / 2) { return; }
+            if (list[0] == 0 || list[list.Length - 1] == 1) { return; }
+
+            int[] nArray3 = new int[list.Length];
+            int t = 0;
+            int r = 0;
+            for (int i = 0; i < nArray3.Length; i++)
+            {
+                nArray3[i] = list[i];
+                if (nArray3[i] == 1) {t++;}
+                if (nArray3[i] == 0) {r++;}
+                if (t < r) { return; }
+            }
+            
 
             char[] ch = new char[list.Length];
             for (int i = 0; i < ch.Length; i++)
